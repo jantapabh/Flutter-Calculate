ff9e780df2b264df2858134857cb4db518d01775
jantapabh <s6035512034@phuket.psu.ac.th>
2020-05-04 21:50:46 +0700

    add calculator

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,15 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'CALCULATOR',
+      title: 'Calculator',
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
-      home: MyCalculatorPage(title: 'CALCULATOR'),
+      home: MyCalculatorPage(title: 'Calculator By Jantapa Binheem 6035512034'),
     );
   }
 }
